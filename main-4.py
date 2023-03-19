5744808765b2a15e5c188c4296406f7f047b82ff
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-03-19 21:37:37 +0200

    Update main-4.py

--- main-4.py
+++ main-4.py
@@ -1,29 +1,26 @@
-def process_jobs():
+def process():
+    # In a nutshell, this code works like Tetris
+    threads = [0] * n_threads
+    output = []
     
-    thread_times = [0] * n_threads
-    job_start_times = []
-
     for job_duration in jobs:
+        # Find the thread with the shortest processing time
+        thread_i = threads.index(min(threads))
         
-        thread_index = thread_times.index(min(thread_times))
+        # Save when the job will start
+        output.append((thread_i, threads[thread_i]))
+        
+        # Add the job duration to the thread's processing time
+        threads[thread_i] += job_duration
 
-       
-        job_start_times.append((thread_index, thread_times[thread_index]))
+    assert n_jobs == len(output)
 
-       
-        thread_times[thread_index] += job_duration
+    print("\n".join([f"{thread_num} {starts_at}" for thread_num, starts_at in output]))
 
-    assert n_jobs == len(job_start_times)
+threads_and_jobs = input().split(" ")
+n_threads = int(threads_and_jobs[0])
+n_jobs = int(threads_and_jobs[1])
+jobs = [int(job) for job in input().split(" ")]
 
-    
-    for thread_num, starts_at in job_start_times:
-        print(f"{thread_num} {starts_at}")
+process()
 
-# Get input from user
-num_threads, num_jobs = input().split()
-n_threads = int(num_threads)
-n_jobs = int(num_jobs)
-jobs = list(map(int, input().split()))
-
-# Process jobs and print output
-process_jobs()
